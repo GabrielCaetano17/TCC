0ee6faaa1d22a1fc4d6bce4cc451ef360378ddea
Gabriel <biellcaetano11@gmail.com>
2024-10-07 21:31:50 -0300

    Testa tela splash

---
+++ app/src/main/java/com/example/tcc/Splash.java
@@ -0,0 +1,28 @@
+package com.example.tcc;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity{
+        int tempoSplash = 3000;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_splash);
+            getSupportActionBar().hide();
+            ChamaTelaLogin();
+        }
+        public void ChamaTelaLogin(){
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent chamaTela = new Intent(Splash.this, MainActivity.class);
+                    startActivity(chamaTela);
+                    finish();
+
+                }
+            }, tempoSplash);
+        }
+    }
